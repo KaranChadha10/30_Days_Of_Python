6a1052ac8d170c6585eaac8e093edd45e957666d
Karan Chadha <karan10chadha@gmail.com>
2025-08-19 22:20:50 +0530

    first day of python

---
+++ 01_Day_Of_Python_HelloWorld.py
@@ -0,0 +1,25 @@
+#Introduction
+# Day 1 - 30DaysOfPython Challenge
+
+print('Hello world!')
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2) #returns quotient
+print(3 ** 2) #exponential
+print(3 % 2) #returns remainder
+print(3 // 2) # Floor division operator(//)
+
+#checking data types
+print(type(5)) #Int
+print(type(3.14)) #Float
+print(type(1 + 3j)) # Complex
+print(type('python')) # String
+print(type([1, 2, 3])) #List
+print(type({'name': 'python'})) #Dictionary
+print(type({1.2, 9.8, 3.14})) #Set
+print(type((1.2, 9.8, 3.14))) #Tuple
+print(type(3 == 3)) #Bool
+print(type(3 >= 3)) #Bool
+
+
